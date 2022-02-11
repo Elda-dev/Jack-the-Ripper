eff3f61980261daec952ba361c7728c0e9b77480
whoIsEllie <lukaskelemen@gmail.com>
2022-02-11 19:46:09 +0000

    Testing metadata and new mp4 to mp3 conversions

--- ripper.py
+++ ripper.py
@@ -1,11 +1,13 @@
 from pytube import YouTube
 from pytube import Search
+import eyed3
+from moviepy.editor import *
 import os
 
 
-def DownloadMusic(songname, destination_path, filename):
+def DownloadMusic(songname, artist, album, destination_path, filename):
     # grabbing the video ID of the top user search
-    s = Search(songname)
+    s = Search(songname + " by " + artist)
     results = s.results
     videoid = results[0].video_id
     # querying youtube and obtaining the video
@@ -24,9 +26,27 @@ def DownloadMusic(songname, destination_path, filename):
             stream = yt.streams.get_by_itag(element.itag)
     # setting the output path - if none entered, use this
     # grabbing the file
+    pathname = yt.title
     out_file = stream.download(output_path=destination_path)
     # encoding the file as a .mp3
-    base, ext = os.path.splitext(out_file)
-    filename = filename.replace("/", ".")
-    new_file = "./Output/" + filename + '.mp3'
-    os.rename(out_file, new_file)
+    mp4path = destination_path + "/" + pathname + ".mp4"
+    print(mp4path)
+    mp3path = destination_path + "/" + songname + ".mp3"
+    print(mp3path)
+    mp4_to_mp3(mp4path, mp3path)
+    # adding tags
+    song = eyed3.load(mp3path)
+    song.initTag()
+    song.tag.artist = artist
+    song.tag.album = album
+    print(song.tag.artist)
+    print(song.tag.album)
+    os.remove(mp4path)
+
+
+#print(EasyID3.valid_keys.keys())
+
+def mp4_to_mp3(mp4, mp3):
+    mp4_without_frames = AudioFileClip(mp4)
+    mp4_without_frames.write_audiofile(mp3)
+    mp4_without_frames.close() # function call mp4_to_mp3("my_mp4_path.mp4", "audio.mp3")
--- single_song.py
+++ single_song.py
@@ -1,6 +1,8 @@
 import ripper
 
 song_name = input("Pick a song to download:    ")
+artist = input("What artist is the song by?: ")
+album = input("What album is it from?: ")
 print("Enter the destination (leave blank for current directory)")
 destination = str(input(">> ")) or './Output'
-ripper.DownloadMusic(song_name, destination, song_name)
+ripper.DownloadMusic(song_name, artist, album, destination, song_name)
